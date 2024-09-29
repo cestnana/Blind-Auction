c624a5ba094b8d90ef7d3bfc89838a9897453e4a
Remi Sha <cestnana@gmail.com>
2024-09-30 00:05:27 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# main
+
+# Get User Input: Name
+# Get User Input: Bid Price
+
+# Ask if there is another bid to add
+
+# If yes: 
+# 1. Clear the screen
+# 2. Go back to the User Input part
+
+# If no: 
+# 1. compare the bids
+# 2. show the {winner} with {price}
